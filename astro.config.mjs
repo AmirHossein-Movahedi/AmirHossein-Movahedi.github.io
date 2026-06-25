038ff29ae9d488fc5b26d7c0043f214b65e3323c
AmirHosseinMovahedi <112428778+AmirHossein-Movahedi@users.noreply.github.com>
2026-06-25 13:52:12 +0330

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Amirhossein-movahedi.github.io',
-  // NO 'base' property needed for user pages!
-  outDir: './dist',
+  output: 'static',
 });
